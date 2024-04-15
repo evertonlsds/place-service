49ee52a65d4a028577acc3163809cb90030eecc6
evertonlsds <everton0102@gmail.com>
2024-04-15 17:48:45 -0300

    config controller

---
+++ src/main/java/br/com/placeservice/place/web/PlaceController.java
@@ -0,0 +1,30 @@
+package br.com.placeservice.place.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.placeservice.place.domain.Place;
+import br.com.placeservice.place.domain.PlaceService;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/places")
+public class PlaceController {
+
+    private PlaceService placeService;
+
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Mono<Place>> create(Place place){
+        var createdPlace = placeService.create(place);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPlace);
+    }
+
+}
