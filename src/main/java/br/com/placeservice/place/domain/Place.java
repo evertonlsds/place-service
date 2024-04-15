ddf532faf1a659d2c76ed567c7fafdf459f82c17
evertonlsds <everton0102@gmail.com>
2024-04-15 17:23:36 -0300

    config service and repository

---
+++ src/main/java/br/com/placeservice/place/domain/Place.java
@@ -0,0 +1,8 @@
+package br.com.placeservice.place.domain;
+
+import java.time.LocalDateTime;
+
+public record Place(
+    Long id, String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updateAT) {
+    
+}
