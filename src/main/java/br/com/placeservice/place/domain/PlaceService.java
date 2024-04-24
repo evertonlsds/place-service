4ba36af112b92e8571b114b3f1e7fe91012ef59d
evertonlsds <everton0102@gmail.com>
2024-04-24 15:19:08 -0300

    slug improvement

--- src/main/java/br/com/placeservice/place/domain/PlaceService.java
+++ src/main/java/br/com/placeservice/place/domain/PlaceService.java
@@ -1,17 +1,21 @@
 package br.com.placeservice.place.domain;
 
+import com.github.slugify.Slugify;
+
 import br.com.placeservice.place.api.PlaceRequest;
 import reactor.core.publisher.Mono;
 
 public class PlaceService {
     private PlaceRepository placeRepository;
+    private Slugify slg;
 
     public PlaceService(PlaceRepository placeRepository) {
         this.placeRepository = placeRepository;
+        this.slg = Slugify.builder().build();
     }
 
     public Mono<Place> create(PlaceRequest placeRequest){
-        var place = new Place(null, placeRequest.name(), placeRequest.slug(), placeRequest.state(), placeRequest.createdAt(), placeRequest.updateAt());
+        var place = new Place(null, placeRequest.name(), slg.slugify(placeRequest.name()), placeRequest.state(), placeRequest.createdAt(), placeRequest.updateAt());
         return placeRepository.save(place);
     }
 }
